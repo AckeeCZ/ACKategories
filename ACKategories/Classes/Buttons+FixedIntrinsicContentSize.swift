b6ca967096e6f0df7a35eff7248959f54a99a2c1
Jan Mísař <misar.jan@gmail.com>
2016-08-31 23:00:12 +0200

    Add title edge insets to intrinsic content size.

---
+++ ACKategories/Classes/Buttons+FixedIntrinsicContentSize.swift
@@ -0,0 +1,18 @@
+//
+//  Buttons+FixedIntrinsicContentSize.swift
+//  Pods
+//
+//  Created by Jan Mísař on 31.08.16.
+//
+//
+
+import UIKit
+
+extension UIButton {
+    public override func intrinsicContentSize() -> CGSize {
+        let size = super.intrinsicContentSize()
+        let width = size.width + self.titleEdgeInsets.left + self.titleEdgeInsets.right
+        let height = size.height + self.titleEdgeInsets.top + self.titleEdgeInsets.bottom
+        return CGSize(width: width, height: height)
+    }
+}
