36b9b548c71fe95b461e7a32f720dd50f148ca4b
Jan Mísař <misar.jan@gmail.com>
2016-09-20 12:58:49 +0200

    Make TableHeaderFooterView public.

--- ACKategories/Classes/TableHeaderFooterView.swift
+++ ACKategories/Classes/TableHeaderFooterView.swift
@@ -1,21 +1,25 @@
+
 /// This view will autolayout its height, even when used as a tableHeaderView or tableFooterView.
 
-class TableHeaderFooterView: UIView {
+public class TableHeaderFooterView: UIView {
+
     private var tableView: UITableView? {
         return superview as? UITableView
     }
+
     private var status: Status {
         return self == tableView?.tableHeaderView ? .Header :
             self == tableView?.tableFooterView ? .Footer :
             .None
     }
+
     private enum Status {
         case Header // is set as tableHeaderView
         case Footer // is set as tableFooterView
         case None // is used as normal view elsewhere
     }
 
-    override func layoutSubviews() {
+    override public func layoutSubviews() {
         super.layoutSubviews()
         switch status {
         case .Header: defer { tableView?.tableHeaderView = self }
