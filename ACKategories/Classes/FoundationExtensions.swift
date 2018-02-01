00278039b9a0d74b25b43f97f40c3dbd858c7c0a
Jan Mísař <misar.jan@gmail.com>
2018-02-01 17:09:04 +0100

    isEmpty on optional collection

--- ACKategories/Classes/FoundationExtensions.swift
+++ ACKategories/Classes/FoundationExtensions.swift
@@ -56,6 +56,17 @@ extension Optional where Wrapped == String {
     }
 }
 
+extension Optional where Wrapped == Collection {
+    public var isEmpty: Bool {
+        switch self {
+        case .none:
+            return true
+        case .some(let value):
+            return value.isEmpty
+        }
+    }
+}
+
 extension NumberFormatter {
     public func string(from number: Int) -> String? {
         return self.string(from: NSNumber(value: number))
