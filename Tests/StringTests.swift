213033d94b61a81a388a0462272b1c901887f274
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-02-06 13:59:51 +0100

    Remove Quick and Nimble from StringTests

--- Tests/StringTests.swift
+++ Tests/StringTests.swift
@@ -6,33 +6,26 @@
 //  Copyright © 2016 CocoaPods. All rights reserved.
 //
 
-//import Quick
-//import Nimble
-//import ACKategories
-//
-//class StringSpec: QuickSpec {
-//    override func spec() {
-//        describe("String") {
-//            it("reads first letter") {
-//                let string = "String"
-//
-//                expect(string.firstLetter) == "S"
-//                expect(string.firstLetter) != "s"
-//            }
-//
-//            it("trimms") {
-//                let string = "     String\nString\n\n\n"
-//
-//                expect(string.trimmed()) == "String\nString"
-//            }
-//
-//
-//            it("normalizes") {
-//                let string = "ěščřžýáíéüä"
-//
-//                expect(string.normalized()) == "escrzyaieua"
-//            }
-//        }
-//    }
-//}
+import XCTest
+import ACKategories
 
+final class StringTests: XCTestCase {
+    func testStringReadsFirstLetter() {
+        let string = "String"
+        
+        XCTAssertEqual(string.firstLetter, "S")
+        XCTAssertNotEqual(string.firstLetter, "s")
+    }
+    
+    func testStringTrimms() {
+        let string = "     String\nString\n\n\n"
+        
+        XCTAssertEqual(string.trimmed(), "String\nString")
+    }
+    
+    func testStringNormalizes() {
+        let string = "ěščřžýáíéüä"
+        
+        XCTAssertEqual(string.normalized(), "escrzyaieua")
+    }
+}
