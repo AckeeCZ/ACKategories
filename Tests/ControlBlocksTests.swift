9848be26dc6438fa976e4728a3b2a064238538c4
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-02-06 14:11:28 +0100

    Remove Quick and Nimble from ControlBlocksTests

--- Tests/ControlBlocksTests.swift
+++ Tests/ControlBlocksTests.swift
@@ -4,120 +4,91 @@ import ACKategories
 
 // Don't know why those tests started to fail
 
-//final class ControlBlocksTests: XCTestCase {
-//
-//    func testHandlerRunsOnTap() {
-//        let v = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
-//        v.addSubview(button)
-//
-//        async { expectation in
-//            button.on(.touchUpInside) { sender in
-//                expectation.fulfill()
-//            }
-//
-//            button.sendActions(for: .touchUpInside)
-//        }
-//    }
-//
-//}
+final class ControlBlocksTests: XCTestCase {
 
-//class ControlBlocksSpec: QuickSpec {
-//    override func spec() {
-//
-//        describe("Button") {
-//            it("runs handler on tap") {
-//
-//                var firstCalled = false
-//
-//                let button = UIButton()
-//                button.on(.touchUpInside) { sender in
-//                    firstCalled = true
-//                }
-//
-//                button.sendActions(for: .touchUpInside)
-//
-//                expect(firstCalled) == true
-//            }
-//
-//            it("runs handlers for all registered events") {
-//
-//                var firstCalled = false
-//                var secondCalled = false
-//
-//                let button = UIButton()
-//                button.on(.touchUpInside) { sender in
-//                    firstCalled = true
-//                }
-//                button.on(.touchDown) { sender in
-//                    secondCalled = true
-//                }
-//
-//                button.sendActions(for: .touchUpInside)
-//                expect(firstCalled) == true
-//
-//                button.sendActions(for: .touchDown)
-//                expect(secondCalled) == true
-//            }
-//
-//            it("holds only one handler for particular event") {
-//
-//                var firstCalled = false
-//                var secondCalled = false
-//
-//                let button = UIButton()
-//                button.on(.touchUpInside) { sender in
-//                    firstCalled = true
-//                }
-//                button.on(.touchUpInside) { sender in
-//                    secondCalled = true
-//                }
-//
-//                button.sendActions(for: .touchUpInside)
-//
-//                expect(firstCalled) == false
-//                expect(secondCalled) == true
-//            }
-//
-//            it("runs one handler for more events") {
-//
-//                var x = 0
-//
-//                let button = UIButton()
-//                button.on([.touchUpInside, .touchDown]) { sender in
-//                    x += 1
-//                }
-//                button.on(.touchUpInside) { sender in
-//                    x += 1
-//                }
-//
-//                button.sendActions(for: .touchUpInside)
-//                button.sendActions(for: .touchDown)
-//                expect(x) == 3
-//
-//                button.sendActions(for: .touchUpInside)
-//                expect(x) == 5
-//            }
-//
-//            it("can unregister action block") {
-//
-//                var firstCalled = false
-//
-//                let button = UIButton()
-//                button.on(.touchUpInside) { sender in
-//                    firstCalled = true
-//                }
-//
-//                button.sendActions(for: .touchUpInside)
-//                expect(firstCalled) == true
-//
-//                firstCalled = false
-//                button.off(.touchUpInside)
-//
-//                button.sendActions(for: .touchUpInside)
-//                expect(firstCalled) == false
-//            }
-//        }
-//    }
-//}
+    func testHandlerRunsOnTap() {
+        var firstCalled = false
+        let button = UIButton()
+        
+        button.on(.touchUpInside) { _ in
+            firstCalled = true
+        }
+        
+        button.sendActions(for: .touchUpInside)
+        XCTAssertTrue(firstCalled)
+    }
 
+    func testHandlerRunsOnAllRegisteredEvents() {
+        var firstCalled = false
+        var secondCalled = false
+        
+        let button = UIButton()
+        button.on(.touchUpInside) { sender in
+            firstCalled = true
+        }
+        button.on(.touchDown) { sender in
+            secondCalled = true
+        }
+        
+        button.sendActions(for: .touchUpInside)
+        XCTAssertTrue(firstCalled)
+        
+        button.sendActions(for: .touchDown)
+        XCTAssertTrue(secondCalled)
+    }
+    
+    func testHoldsOnlyOneHandlerForParticularEvent() {
+        var firstCalled = false
+        var secondCalled = false
+        
+        let button = UIButton()
+        button.on(.touchUpInside) { sender in
+            firstCalled = true
+        }
+        button.on(.touchUpInside) { sender in
+            secondCalled = true
+        }
+        
+        button.sendActions(for: .touchUpInside)
+        
+        XCTAssertFalse(firstCalled)
+        XCTAssertTrue(firstCalled)
+    }
+    
+    func testRunsOneHandlerForMoreEvents() {
+        var x = 0
+        
+        let button = UIButton()
+        button.on([.touchUpInside, .touchDown]) { sender in
+            x += 1
+        }
+        button.on(.touchUpInside) { sender in
+            x += 1
+        }
+        
+        button.sendActions(for: .touchUpInside)
+        button.sendActions(for: .touchDown)
+        XCTAssertEqual(3, x)
+        
+        button.sendActions(for: .touchUpInside)
+        XCTAssertEqual(5, x)
+    }
+    
+    func testCanUnregisterActionBlock() {
+        var firstCalled = false
+        
+        let button = UIButton()
+        button.on(.touchUpInside) { sender in
+            firstCalled = true
+        }
+        
+        button.sendActions(for: .touchUpInside)
+        XCTAssertTrue(firstCalled)
+        
+        firstCalled = false
+        button.off(.touchUpInside)
+        
+        button.sendActions(for: .touchUpInside)
+        XCTAssertFalse(firstCalled)
+    }
+}
