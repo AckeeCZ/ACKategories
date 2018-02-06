0655071834f0e5af821733a13cfe446ef2ada50b
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-02-06 16:57:04 +0100

    Add UIButtonExtensions - centerVertically extension

---
+++ ACKategories/UIButtonExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExtensions.swift
+//  ACKategories
+//
+//  Created by Jakub Olejník on 06/02/2018.
+//  Copyright © 2018 Josef Dolezal. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func centerVertically(padding: CGFloat = 6.0) {
+        guard let imageSize = imageView?.frame.size, let titleSize = titleLabel?.frame.size else { return }
+        let totalHeight = imageSize.height + titleSize.height + padding
+        
+        imageEdgeInsets = UIEdgeInsets(top: -(totalHeight - imageSize.height), left: 0, bottom: 0, right: -titleSize.width)
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageSize.width, bottom: -(totalHeight - titleSize.height), right: 0)
+    }
+}
