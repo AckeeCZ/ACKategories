baccda91586719829be9b89b0f32689f4c443d18
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-06-13 14:42:24 +0200

    Add new `Date` extensions

--- ACKategories/UIStackViewExtensions.swift
+++ ACKategories/UIStackViewExtensions.swift
@@ -2,6 +2,7 @@ import UIKit
 
 @available(iOS 9.0, *)
 extension UIStackView {
+    /// Remove all arranged subviews
     public func removeAllArrangedSubviews() {
         arrangedSubviews.forEach { removeArrangedSubview($0); $0.removeFromSuperview() }
     }
