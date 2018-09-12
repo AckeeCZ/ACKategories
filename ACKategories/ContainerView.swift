bae1e45ef1380f87a8f24356346bd803cbec9f1e
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-09-12 17:52:00 +0200

    Add `ContainerView`

---
+++ ACKategories/ContainerView.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+@available(iOSApplicationExtension 9.0, *)
+public final class ContainerView<ContainedView: UIView>: UIView {
+    
+    private(set) weak var containedView: ContainedView!
+    
+    // MARK: Initializers
+    
+    public init(view: ContainedView) {
+        super.init(frame: .zero)
+        
+        addSubview(view)
+        NSLayoutConstraint.activate([
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor),
+            ])
+        containedView = view
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
