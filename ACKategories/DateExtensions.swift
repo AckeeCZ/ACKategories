fea9e1f2ddcbd7c1abe068e282c24f317840f76e
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-10-04 17:36:47 +0200

    Remove DateExtensions conflicting with DateTools

--- ACKategories/DateExtensions.swift
+++ ACKategories/DateExtensions.swift
@@ -1,24 +1,6 @@
 import Foundation
 
-extension Date {
-    /// Seconds from date
-    public var second: Int { return Calendar.current.component(.second, from: self) }
-    
-    /// Minutes from date
-    public var minute: Int { return Calendar.current.component(.minute, from: self) }
-    
-    /// Hours from date in 24-hour format
-    public var hour: Int { return Calendar.current.component(.hour, from: self) }
-    
-    /// Days from date
-    public var day: Int { return Calendar.current.component(.day, from: self) }
-    
-    /// Months from date
-    public var month: Int { return Calendar.current.component(.month, from: self) }
-    
-    /// Year from date
-    public var year: Int { return Calendar.current.component(.year, from: self) }
-    
+extension Date {    
     /// String from date formatted with given template
     ///
     /// Uses cached date formatter, so no need to worry about performance. See `DateFormatter.cached(for template:)`
