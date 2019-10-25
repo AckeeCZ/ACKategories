eaea6bab0daf4b9724fc8cd55c02a0f066c03e5d
Marek Fořt <marek.fort@ackee.cz>
2019-10-25 14:21:33 +0200

    Change all to iOS for SPM compatibility.

--- Tapestries/Sources/TapestryConfig/TapestryConfig.swift
+++ Tapestries/Sources/TapestryConfig/TapestryConfig.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let config = TapestryConfig(release: Release(actions: [.pre(.docsUpdate),
-                                                       .pre(.dependenciesCompatibility([.cocoapods, .carthage, .spm(.all)]))],
+                                                       .pre(.dependenciesCompatibility([.cocoapods, .carthage, .spm(.iOS)]))],
                                              add: ["README.md",
                                                    "ACKategories.podspec",
                                                    "CHANGELOG.md"],
                                              commitMessage: "Version \(Argument.version)",
-                                             push: false))
+                                             push: true))
