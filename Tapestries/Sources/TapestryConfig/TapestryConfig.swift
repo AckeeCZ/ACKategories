caee111be3cbc5eb5eb0c402c5cc3236fd0c53f8
Marek Fořt <marek.fort@ackee.cz>
2019-10-25 14:12:13 +0200

    Setup tapestry.

---
+++ Tapestries/Sources/TapestryConfig/TapestryConfig.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let config = TapestryConfig(release: Release(actions: [.pre(.docsUpdate),
+                                                       .pre(.dependenciesCompatibility([.cocoapods, .carthage, .spm(.all)]))],
+                                             add: ["README.md",
+                                                   "ACKategories.podspec",
+                                                   "CHANGELOG.md"],
+                                             commitMessage: "Version \(Argument.version)",
+                                             push: false))
