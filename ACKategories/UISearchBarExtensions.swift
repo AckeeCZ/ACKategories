9ef89f3ef2c6ac1501c626fd4909bd3512c2cabf
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-12-08 23:32:40 +0100

    ♻️ Deprecate `UISearchBar.textField` in iOS 13 in favor of `searchTextField`

--- ACKategories/UISearchBarExtensions.swift
+++ ACKategories/UISearchBarExtensions.swift
@@ -5,6 +5,7 @@ public extension UISearchBar {
     ///
     /// On iOS 13+ this just calls `searchTextField`, and if you're targeting iOS 13+,
     /// you should use that as you get `UISearchTextField` instead of just `UITextField`.
+    @available(iOS, deprecated: 13, renamed: "searchTextField")
     var textField: UITextField! {
         if #available(iOS 13, *) {
             return searchTextField
