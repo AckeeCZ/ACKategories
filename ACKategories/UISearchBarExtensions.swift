b317f48f616b5e092c2e65d5c883999e3a7ab27a
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-12-08 20:54:54 +0100

    ♻️ Use native `searchTextField` for `UISearchBar.textField`

--- ACKategories/UISearchBarExtensions.swift
+++ ACKategories/UISearchBarExtensions.swift
@@ -2,5 +2,13 @@ import UIKit
 
 public extension UISearchBar {
     /// Shorthand for contained textField
-    var textField: UITextField! { return value(forKey: "searchField") as? UITextField }
+    ///
+    /// On iOS 13+ this just calls `searchTextField`, and if you're targeting iOS 13+,
+    /// you should use that as you get `UISearchTextField` instead of just `UITextField`.
+    var textField: UITextField! {
+        if #available(iOS 13, *) {
+            return searchTextField
+        }
+        return value(forKey: "searchField") as? UITextField
+    }
 }
