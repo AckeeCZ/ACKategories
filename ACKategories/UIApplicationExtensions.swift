63a929cd5567f070b81cc1570e4d3196ce4538fe
Igor Rosocha <igor.rosocha@ackee.cz>
2019-12-16 17:02:05 +0100

    ✨ Implement method to completely clear launch screen cache.

---
+++ ACKategories/UIApplicationExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIApplicationExtensions.swift
+//  ACKategories
+//
+//  Created by Igor Rosocha on 12/16/19.
+//  Copyright © 2019 Ackee, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+public extension UIApplication {
+    /// Completely clear launch screen cache
+    func clearLaunchScreenCache() {
+        do {
+            try FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
+        } catch {
+            print("Failed to delete launch screen cache: \(error)")
+        }
+    }
+}
