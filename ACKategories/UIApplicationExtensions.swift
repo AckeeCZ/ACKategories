c5df4b5516e38f21ce06939ad4fc6a512d8e861d
Igor Rosocha <igor.rosocha@ackee.cz>
2019-12-17 14:27:59 +0100

    💡 Update documentation.

--- ACKategories/UIApplicationExtensions.swift
+++ ACKategories/UIApplicationExtensions.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 public extension UIApplication {
-    /// Completely clear launch screen cache
+    /// The system caches launch images and is really bad at clearing it, even after the app has been deleted and re-launched,
+    /// which causes the launch screen not to display new/correct images.
+    /// This method completely clears launch screen cache.
+    ///
+    /// You can put it in your app’s initialization code behind an argument flag that you enable during launch screen development,
+    /// then leave it disabled when you’re not working on your launch screen.
+    ///
+    /// Source: https://rambo.codes/ios/quick-tip/2019/12/09/clearing-your-apps-launch-screen-cache-on-ios.html
     func clearLaunchScreenCache() {
         do {
             try FileManager.default.removeItem(atPath: NSHomeDirectory() + "/Library/SplashBoard")
