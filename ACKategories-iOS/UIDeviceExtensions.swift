c5314724c8d901310ce405f14bc590a6d7e95ff3
Jakub Olejník <jakub.olejnik@ackee.cz>
2020-02-17 12:54:54 +0100

    ✨ Add `device.modelName` extension

--- ACKategories-iOS/UIDeviceExtensions.swift
+++ ACKategories-iOS/UIDeviceExtensions.swift
@@ -13,4 +13,16 @@ extension UIDevice {
     public var isPad: Bool {
         return userInterfaceIdiom == .pad
     }
+    
+    /// Returns device model name e.g. "iPhone11,6" for XS Max
+    var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }
 }
