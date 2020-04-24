6ef1899229672a391b6110be458217212bcdf0e8
Jakub Olejník <jakub.olejnik@ackee.cz>
2020-04-24 13:26:36 +0200

    ✨ Prepare prototype cell storage to `UITableView`

--- ACKategories-iOS/ReusableView.swift
+++ ACKategories-iOS/ReusableView.swift
@@ -6,13 +6,38 @@ extension UITableViewCell: Reusable { }
 extension UITableViewHeaderFooterView: Reusable { }
 extension UICollectionReusableView: Reusable { }
 
+private enum Keys {
+    static var prototypeCellStorage = UInt8(0)
+}
+
 extension UITableView {
+    /// Storage for prototype cells
+    private var prototypeCells: [String: UITableViewCell] {
+        get {
+            objc_getAssociatedObject(self, &Keys.prototypeCellStorage) as? [String: UITableViewCell] ?? [:]
+        }
+        set {
+            objc_setAssociatedObject(self, &Keys.prototypeCellStorage, newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+    
     /// Dequeues `UITableViewCell` generically according to expression result type
     ///
     /// Cell doesn't need to be registered as this method registers it before use.
     public func dequeueCell<T>(for indexPath: IndexPath, type: T.Type = T.self) -> T where T: UITableViewCell {
         register(T.classForCoder(), forCellReuseIdentifier: T.reuseIdentifier)
         return dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as! T
+    }
+    
+    /// Get prototype cell of given type
+    public func prototypeCell<T>(type: T.Type = T.self) -> T where T:UITableViewCell {
+        if let prototype = prototypeCells[T.reuseIdentifier] as? T {
+            return prototype
+        }
+        
+        let prototype = T()
+        prototypeCells[T.reuseIdentifier] = prototype
+        return prototype
     }
 
     /// Dequeues `UITableViewHeaderFooterView` generically according to expression result type
