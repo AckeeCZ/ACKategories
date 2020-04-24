a4e03b4046d5882f9f9eb4bcb2f6fe4ebe38ec41
Jakub Olejník <jakub.olejnik@ackee.cz>
2020-04-24 13:27:36 +0200

    ✨ Add prototype cell storage for `UICollectionView`

--- ACKategories-iOS/ReusableView.swift
+++ ACKategories-iOS/ReusableView.swift
@@ -30,7 +30,7 @@ extension UITableView {
     }
     
     /// Get prototype cell of given type
-    public func prototypeCell<T>(type: T.Type = T.self) -> T where T:UITableViewCell {
+    public func prototypeCell<T>(type: T.Type = T.self) -> T where T: UITableViewCell {
         if let prototype = prototypeCells[T.reuseIdentifier] as? T {
             return prototype
         }
@@ -50,12 +50,33 @@ extension UITableView {
 }
 
 extension UICollectionView {
+    /// Storage for prototype cells
+    private var prototypeCells: [String: UICollectionViewCell] {
+        get {
+            objc_getAssociatedObject(self, &Keys.prototypeCellStorage) as? [String: UICollectionViewCell] ?? [:]
+        }
+        set {
+            objc_setAssociatedObject(self, &Keys.prototypeCellStorage, newValue, .OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+    
     /// Dequeues `UICollectionViewCell` generically according to expression result type
     ///
     /// Cell doesn't need to be registered as this method registers it before use.
     public func dequeueCell<T>(for indexPath: IndexPath, type: T.Type = T.self) -> T where T: UICollectionViewCell {
         register(T.classForCoder(), forCellWithReuseIdentifier: T.reuseIdentifier)
         return dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as! T
+    }
+    
+    /// Get prototype cell of given type
+    public func prototypeCell<T>(type: T.Type = T.self) -> T where T: UICollectionViewCell {
+        if let prototype = prototypeCells[T.reuseIdentifier] as? T {
+            return prototype
+        }
+        
+        let prototype = T()
+        prototypeCells[T.reuseIdentifier] = prototype
+        return prototype
     }
 
     /// Dequeues `UICollectionReusableView` generically according to expression result type
