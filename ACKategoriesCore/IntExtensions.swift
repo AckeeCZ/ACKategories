7fbfa606af3d75231b07713ac9d8dbb24bf2b773
Jakub Olejnik <jakub.olejnik@ackee.cz>
2021-11-09 23:07:33 +0100

    ✨ Add safe inits to convert `Double` to `Int`

---
+++ ACKategoriesCore/IntExtensions.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public extension Int {
+    /// Optionally create `Int` checking if `double` is not Nan nor infinity
+    init?(safe double: Double) {
+        if double.isNaN || double.isInfinite {
+            return nil
+        }
+        
+        self.init(double)
+    }
+    
+    /// Optionally create `Int` checking if `float` is not Nan nor infinity
+    init?(safe float: Float) {
+        if float.isNaN || float.isInfinite {
+            return nil
+        }
+        
+        self.init(float)
+    }
+    
+    /// Optionally create `Int` checking if `cgFloat` is not Nan nor infinity
+    init?(safe cgFloat: CGFloat) {
+        if cgFloat.isNaN || cgFloat.isInfinite {
+            return nil
+        }
+        
+        self.init(cgFloat)
+    }
+}
