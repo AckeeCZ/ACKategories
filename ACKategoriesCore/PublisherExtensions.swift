318771fea2e9bba277c44d735b3740b79c47b136
Vendula Švastalová <vendula.svastalova@ackee.cz>
2022-03-17 15:30:25 +0100

    ✨ Make PublisherExtensions available from iOS 13.0

--- ACKategoriesCore/PublisherExtensions.swift
+++ ACKategoriesCore/PublisherExtensions.swift
@@ -1,6 +1,6 @@
 import Combine
 
-@available(macOS 10.15, *)
+@available(macOS 10.15, iOS 13.0, *)
 extension Publisher where Failure == Never {
     /// Weakifies `Root` object and assigns `Output` to `keyPath` on `Root`.
     public func weakAssign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on object: Root) -> AnyCancellable {
