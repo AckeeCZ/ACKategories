5ef618c2f70fe366ac109fc2efb6d72bddc2e664
Jakub Olejnik <jakub.olejnik@ackee.cz>
2023-11-02 20:39:45 +0100

    ⬆️ Update CI

--- TapestryConfig.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let config = TapestryConfig(release: Release(actions: [.pre(.docsUpdate),
-                                                       .pre(.dependenciesCompatibility([.cocoapods, .carthage, .spm(.all)]))],
-                                             add: ["README.md",
-                                                   "ACKategories.podspec",
-                                                   "CHANGELOG.md"],
-                                             commitMessage: "Version \(Argument.version)",
-                                             push: true))
