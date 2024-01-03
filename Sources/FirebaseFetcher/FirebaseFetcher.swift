ade796c2c6c40338a28a543e88b3c8b947a71982
Jakub Olejnik <jakub.olejnik@ackee.cz>
2024-01-03 14:33:50 +0100

    ⬆️ Use Xcode 15.1

--- Sources/FirebaseFetcher/FirebaseFetcher.swift
+++ Sources/FirebaseFetcher/FirebaseFetcher.swift
@@ -1,6 +1,7 @@
 import ACKategories
 import FirebaseRemoteConfig
 
+@available(macOS 10.15.0, *)
 public final class FirebaseFetcher: MinBuildNumberFetcher {
     public var minBuildNumber: Int {
         get async throws {
