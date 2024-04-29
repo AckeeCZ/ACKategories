e51ad570f11e239d805bc9fd0242ed2ffd73ee4b
Jakub Olejnik <jakub.olejnik@ackee.cz>
2024-04-29 12:07:16 +0200

    ✨ Make lineHeight optional in FontModifier

--- Sources/ACKategories/SwiftUIExtensions/FontModifier.swift
+++ Sources/ACKategories/SwiftUIExtensions/FontModifier.swift
@@ -10,28 +10,22 @@ public extension View {
     ///    - font: The font to use in this view.
     ///    - lineHeight: The line height to use in this view.
     ///    - textStyle: The text style for relative font scaling. If `nil`is specified then dynamic type is turned off.
-    func font(
+    @ViewBuilder func font(
         _ font: UIFont,
-        lineHeight: Double,
+        lineHeight: Double?,
         textStyle: Font.TextStyle?
     ) -> some View {
-        let customFont: Font
-
         // Do not scale font based on dynamic type when nil `relativeTo` specified
-        if let textStyle = textStyle {
-            customFont = .custom(
-                font.fontName,
-                size: font.pointSize,
-                relativeTo: textStyle
-            )
-        } else {
-            customFont = Font(font)
-        }
+        let customFont = textStyle
+            .map { Font.custom(font.fontName, size: font.pointSize, relativeTo: $0) } ?? Font(font)
 
-        return self
-            .font(customFont)
-            .lineSpacing(lineHeight - font.lineHeight)
-            .padding(.vertical, (lineHeight - font.lineHeight) / 2)
+        if let lineHeight {
+            self.font(customFont)
+                .lineSpacing(lineHeight - font.lineHeight)
+                .padding(.vertical, (lineHeight - font.lineHeight) / 2)
+        } else {
+            self.font(customFont)
+        }
     }
 }
 #endif
