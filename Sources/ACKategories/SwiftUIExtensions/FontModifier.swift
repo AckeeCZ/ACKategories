71dba65db71b114ea3b24d749532be7627af94db
Jakub Olejnik <jakub.olejnik@ackee.cz>
2026-01-05 15:24:17 +0100

    Use native lineHeight viewModifier on iOS 26

--- Sources/ACKategories/SwiftUIExtensions/FontModifier.swift
+++ Sources/ACKategories/SwiftUIExtensions/FontModifier.swift
@@ -10,6 +10,8 @@ public extension View {
     ///    - font: The font to use in this view.
     ///    - lineHeight: The line height to use in this view.
     ///    - textStyle: The text style for relative font scaling. If `nil`is specified then dynamic type is turned off.
+    ///
+    /// On iOS 26 and above uses native [lineHeight](https://developer.apple.com/documentation/swiftui/environmentvalues/lineheight) view modifier.
     @ViewBuilder func font(
         _ font: UIFont,
         lineHeight: Double?,
@@ -19,7 +21,10 @@ public extension View {
         let customFont = textStyle
             .map { Font.custom(font.fontName, size: font.pointSize, relativeTo: $0) } ?? Font(font)
 
-        if let lineHeight {
+        if #available(iOS 26.0, tvOS 26.0, watchOS 26.0, *), let lineHeight {
+            self.font(customFont)
+                .lineHeight(.exact(points: lineHeight))
+        } else if let lineHeight {
             self.font(customFont)
                 .lineSpacing(lineHeight - font.lineHeight)
                 .padding(.vertical, (lineHeight - font.lineHeight) / 2)
