2d48cdeb0602aae040492b3de936eae14e2ec5b5
Jindrich Dolezal <jindrich.dolezal@ackee.cz>
2026-09-08 12:37:27 +0200

    🔥 Drop dead @available from tests

--- Tests/NetworkingTests/OAuthInterceptor_Tests.swift
+++ Tests/NetworkingTests/OAuthInterceptor_Tests.swift
@@ -3,7 +3,6 @@ import Networking
 import XCTest
 
 @MainActor
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 final class OAuthInterceptor_Tests: XCTestCase {
     private final class UnexpectedCall: Error {
         init() { XCTFail("Should not be called") }
